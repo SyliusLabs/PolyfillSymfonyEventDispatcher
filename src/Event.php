c3462b8a9ea7d3a82174237258c7fe8fb39eff8c
Grzegorz Sadowski <sadowskigp@gmail.com>
2020-01-31 15:20:35 +0100

    Fix event class

--- src/Event.php
+++ src/Event.php
@@ -7,12 +7,12 @@ namespace SyliusLabs\Polyfill\Symfony\EventDispatcher;
 use Symfony\Contracts\EventDispatcher\Event as ContractEvent;
 use Symfony\Component\EventDispatcher\Event as ComponentEvent;
 
-if (interface_exists(ContractEvent::class)) {
-    class Event extends ContractEvent
+if (class_exists(ComponentEvent::class)) {
+    class Event extends ComponentEvent
     {
     }
 } else {
-    class Event extends ComponentEvent
+    class Event extends ContractEvent
     {
     }
 }
