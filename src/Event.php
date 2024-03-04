13628d3375d8330a0c28239ea54892fea2bf0494
Łukasz Chruściel <lchrusciel@gmail.com>
2024-03-04 13:45:35 +0100

    [Maintenance] Drop polyfiling logic

--- src/Event.php
+++ src/Event.php
@@ -5,14 +5,7 @@ declare(strict_types=1);
 namespace SyliusLabs\Polyfill\Symfony\EventDispatcher;
 
 use Symfony\Contracts\EventDispatcher\Event as ContractEvent;
-use Symfony\Component\EventDispatcher\Event as ComponentEvent;
 
-if (class_exists(ComponentEvent::class)) {
-    class Event extends ComponentEvent
-    {
-    }
-} else {
-    class Event extends ContractEvent
-    {
-    }
+class Event extends ContractEvent
+{
 }
